30e621c2c75225039082709175ed28d824901604
Jeremy <jgertz26@gmail.com>
2015-11-24 23:14:58 -0500

    phones controller functioning, tests passing

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,16 +4,16 @@
 
 describe('PhoneCat controllers', function() {
 
-  describe('PhoneListCtrl', function() {
+  describe('PhoneListCtrl', function(){
 
     beforeEach(module('phonecatApp'));
 
     it('should create "phones" model with 3 phones', inject(function($controller) {
       var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+          ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
-        expect(scope.phones.length).toBe(3);
-      }));
+      expect(scope.phones.length).toBe(3);
+    }));
 
   });
 });
