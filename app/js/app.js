144bb84d5b8e1e26dc7027011bc64d1452029c7e
Jeremy <jgertz26@gmail.com>
2015-11-28 23:37:43 -0500

    modified app.js file

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,20 @@
-'use strict';
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
 
-/* App Module */
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+}]);
